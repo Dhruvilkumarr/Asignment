6a064c2798496798c6a14aab5358db69d52113cf
Dhruvilkumarr <113696748+Dhruvilkumarr@users.noreply.github.com>
2022-09-24 01:00:55 -0400

    Add files via upload

---
+++ Lab Assignment 1/src/UI/lbl_image.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package UI;
+
+/**
+ *
+ * @author dhruvilsuthar
+ */
+class lbl_image {
+
+    static void setIcon(Object object) {
+       
+    }
+
+    static class setIcon {
+
+        public setIcon() {
+        }
+    }
+    
+}
---
+++ Lab Assignment 1/src/model/Product.java
@@ -0,0 +1,221 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.Date;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author dhruvilsuthar
+ */
+public class Product {
+    
+    private String First_Name;
+    private String Last_Name;
+    private String DOB;
+    private String Age;
+    private String Telephone_number;
+    private String Email;
+    private String Sline1;
+    private String Sline2;
+    private String City;
+    private String Country;
+    private String Affiliation;
+    private String Major;
+    private String Career;
+    private String Degree1;
+    private String D1Start;
+    private String D1End;
+    private String Degree2;
+    private String D2Start;
+    private String D2End;
+    private File Photo;
+    
+    //getere
+    
+    
+    public String getFirst_Name() {
+        return First_Name;
+    }
+
+    public String getLast_Name() {
+        return Last_Name;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public String getAge() {
+        return Age;
+    }
+
+    public String getTelephone_number() {
+        return Telephone_number;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public String getSline1() {
+        return Sline1;
+    }
+
+    public String getSline2() {
+        return Sline2;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public String getAffiliation() {
+        return Affiliation;
+    }
+
+    public String getMajor() {
+        return Major;
+    }
+
+    public String getCareer() {
+        return Career;
+    }
+
+    public String getDegree1() {
+        return Degree1;
+    }
+
+    public String getD1Start() {
+        return D1Start;
+    }
+
+    public String getD1End() {
+        return D1End;
+    }
+
+    public String getDegree2() {
+        return Degree2;
+    }
+
+    public String getD2Start() {
+        return D2Start;
+    }
+
+    public String getD2End() {
+        return D2End;
+    }
+
+    public File getPhoto() {
+        return Photo;
+    }
+    
+    
+    // setter
+
+    public void setFirst_Name(String First_Name) {
+        this.First_Name = First_Name;
+    }
+
+    public void setLast_Name(String Last_Name) {
+        this.Last_Name = Last_Name;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public void setAge(String Age) {
+        this.Age = Age;
+    }
+
+    public void setTelephone_number(String Telephone_number) {
+        this.Telephone_number = Telephone_number;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public void setSline1(String Sline1) {
+        this.Sline1 = Sline1;
+    }
+
+    public void setSline2(String Sline2) {
+        this.Sline2 = Sline2;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public void setCountry(String Country) {
+        this.Country = Country;
+    }
+
+    public void setAffiliation(String Affiliation) {
+        this.Affiliation = Affiliation;
+    }
+
+    public void setMajor(String Major) {
+        this.Major = Major;
+    }
+
+    public void setCareer(String Career) {
+        this.Career = Career;
+    }
+
+    public void setDegree1(String Degree1) {
+        this.Degree1 = Degree1;
+    }
+
+    public void setD1Start(String D1Start) {
+        this.D1Start = D1Start;
+    }
+
+    public void setD1End(String D1End) {
+        this.D1End = D1End;
+    }
+
+    public void setDegree2(String Degree2) {
+        this.Degree2 = Degree2;
+    }
+
+    public void setD2Start(String D2Start) {
+        this.D2Start = D2Start;
+    }
+
+    public void setD2End(String D2End) {
+        this.D2End = D2End;
+    }
+
+    public void setPhoto(File Photo) {
+        this.Photo = Photo;
+    }
+
+    public String getName() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+   
+    
+
+    private static class File {
+
+        public File() {
+        }
+    }
+
+    
+    
+    
+    
+    
+}
